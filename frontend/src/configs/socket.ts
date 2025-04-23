d85be16d73285a4553d9c74cafa1edf13598c753
sujith <syjithkpy@gmail.com>
2025-04-23 19:05:13 +0530

    something change in socket front end

--- frontend/src/configs/socket.ts
+++ frontend/src/configs/socket.ts
@@ -1,7 +1,7 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
+export const socket = io(import.meta.env.VITE_API_SOCKET_URL, {
   transports: ["websocket"], // force websocket
   withCredentials: true, // only if backend has credentials: true
   path: "/ws",
